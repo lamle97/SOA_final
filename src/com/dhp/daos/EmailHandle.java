28ea802f6f20c6d45dacf80fb29c361f58056941
Hao Anh Nguyen <haoanguyen@kms.com.vn>
2019-05-13 23:44:35 +0700

    fix email

--- src/com/dhp/daos/EmailHandle.java
+++ src/com/dhp/daos/EmailHandle.java
@@ -64,10 +64,10 @@ public class EmailHandle {
 	public static boolean sendEmail(String email,Order order) {
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "587");
+		props.put("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "587");		
+		props.put("mail.smtp.port", "465");		
 		
 		Session session = Session.getInstance(props,
 			new javax.mail.Authenticator() {
